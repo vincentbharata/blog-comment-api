a428d8fa6cc9af80499b6e5171faafeadbf49f0e
vincentbharata <vincentbharataa@gmail.com>
2025-06-17 10:33:04 +0700

    init

---
+++ src/main/java/com/example/blog_comment/BlogCommentApplication.java
@@ -0,0 +1,13 @@
+package com.example.blog_comment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogCommentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogCommentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blog_comment/BlogCommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blog_comment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogCommentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
